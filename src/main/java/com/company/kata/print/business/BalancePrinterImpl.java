c41a048f0840b5589030517a9892961abf40d085
abhar <abhar@isilis.fr>
2022-11-22 09:29:51 +0100

    :hammer: return not a period when needed

--- src/main/java/com/company/kata/print/business/BalancePrinterImpl.java
+++ src/main/java/com/company/kata/print/business/BalancePrinterImpl.java
@@ -12,6 +12,8 @@ import com.company.kata.print.api.BalancePrinter;
 
 import java.time.LocalDate;
 
+import static com.company.kata.model.type.PeriodType.NOT_A_PERIOD;
+
 /**
  * The type Balance printer.
  */
@@ -39,6 +41,7 @@ public class BalancePrinterImpl implements BalancePrinter {
      */
     public String print(Balance balance, String[] words) {
         PeriodType periodType = periodTypeParser.parse(words[1]);
+        if (NOT_A_PERIOD.equals(periodType)) return NOT_A_PERIOD.name();
         LocalDate date = dateParser.parse(words[2], periodType);
         return balanceCalculator.calculate(balance, periodType, date);
     }
